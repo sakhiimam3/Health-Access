0d9cb0efeea32da9fb7d9653f143b90fa47416df
Sakhidad <61518186+sakhiimam3@users.noreply.github.com>
2025-05-07 16:47:20 +0500

    Refactor PagesWrapper component for improved readability and maintainability; adjust prop formatting and ensure consistent styling for Frequently Asked and PartnerSection components.

--- src/components/layout/pagesWrapper.tsx/index.tsx
+++ src/components/layout/pagesWrapper.tsx/index.tsx
@@ -5,13 +5,26 @@ import FrequentlyAsked from "@/components/frequently-asked";
 import Blogs from "@/components/Blogs";
 import PartnerSection from "@/components/partnerSection";
 
-const 
-PagesWrapper = ({ children ,bgColor,btnColor,type,isSearchPage,isPartner}: { children: React.ReactNode ,bgColor:string ,btnColor:string ,type?:boolean ,isSearchPage?:boolean,isPartner?:boolean }) => {
+const PagesWrapper = ({
+  children,
+  bgColor,
+  btnColor,
+  type,
+  isSearchPage,
+  isPartner,
+}: {
+  children: React.ReactNode;
+  bgColor: string;
+  btnColor: string;
+  type?: boolean;
+  isSearchPage?: boolean;
+  isPartner?: boolean;
+}) => {
   return (
     <div>
       <Header />
       {children}
-      {!isSearchPage && <FrequentlyAsked  btnColor={btnColor} />}
+      {!isSearchPage && <FrequentlyAsked btnColor={btnColor} />}
       {isPartner && <PartnerSection bgColor={bgColor} />}
       {type && <Blogs />}
       <Footer />
